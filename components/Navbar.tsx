886c5d2703e530718eb90ebf13614e9035701bcb
Thiago Rocha <thiago_srocha01@hotmail.com>
2024-04-28 22:27:32 -0300

    feat: adicionando dark mode e implementando  navegação para desktop

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,12 @@
+"use client";
 import React from "react";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { ThemeSwitcherBtn } from "./ThemeSwitcherBtn";
 
 function Navbar() {
   return (
@@ -8,8 +16,58 @@ function Navbar() {
   );
 }
 
+const items = [
+  { label: "Dashboard", link: "/" },
+  { label: "Transactions", link: "/transactions" },
+  { label: "Manage", link: "/manage" },
+];
+
 function DesktopNavbar() {
-  return <div></div>;
+  return (
+    <div className="hidden border-separate border-b bg-background md:block">
+      <nav className="container flex items-center justify-between px-8">
+        <div className="flex min-h-[60px] items-center gap-x-4 h-[80px]">
+          <Logo />
+          <div className="flex h-full">
+            {items.map((item) => (
+              <NavbarItem
+                key={item.label}
+                link={item.link}
+                label={item.label}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function NavbarItem({ link, label }: { link: string; label: string }) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
+
+  return (
+    <div className="flex relative items-center">
+      <Link
+        href={link}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
+          isActive && "text-foreground"
+        )}
+      >
+        {label}
+      </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-x1 bg-foreground md:block" />
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
